4246a9e371123218d9b5f8394267422a81d09b7f
edwardyu99 <eykm.yahoo@gmail.com>
2026-04-23 23:22:35 +0800

    20260423 27247 yuz->yuu

---
+++ py312.bat
@@ -0,0 +1,5 @@
+rem #!/bin/bash
+rem source ~/miniconda3/etc/profile.d/conda.sh
+conda activate py312
+rem ./uvall.sh
+rem jupyter lab --notebook-dir=/mnt/c/Ai
--- uvall.bat
+++ uvall.bat
@@ -2,5 +2,5 @@ rem #!/bin/bash
 rem uv pip freeze | sed 's/==.*$//' | grep -v '@' > requirements.txt
 rem sed -i 's/^ta-lib$/ta-lib==0.4.28/' requirements.txt
 rem sed -i 's/^pystan$/pystan==3.10.0/' requirements.txt
-uv pip install --upgrade -r requirementswin.txt
+uv pip install --upgrade -r requirements.txt
 rem # uv pip install --upgrade -r <(uv pip freeze)
